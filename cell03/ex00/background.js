a10b354aee27ffb757915a7b8fb4c82f19cd7edb
Sornchaix01 <67050525@kmitl.ac.th>
2026-09-16 02:43:15 +0700

    Add cell03 ex00 background

---
+++ cell03/ex00/background.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("changeButton");
+
+button.addEventListener("click", function () {
+    const randomColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    document.body.style.backgroundColor = randomColor;
+});
